27c1be4e004db47c5fa8b479fd87dc81159d6864
Jhon peralta <jhonperalta13@gmail.com>
2023-11-24 19:21:47 -0500

    update cors v3

--- src/main/java/com/alcadia/bovid/Configuration/ApplicationConfig.java
+++ src/main/java/com/alcadia/bovid/Configuration/ApplicationConfig.java
@@ -63,9 +63,6 @@ public class ApplicationConfig {
         return new JwtAuthFilter(jwtAuthenticationProvider);
     }
 
-    @Bean
-    public RequestMappingHandlerMapping requestMappingHandlerMapping() {
-        return new RequestMappingHandlerMapping();
-    }
+    
 
 }
