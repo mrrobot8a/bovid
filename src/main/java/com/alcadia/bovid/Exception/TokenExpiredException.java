6a49a047d4d682fbf92311bddb7845bdadeb2725
Jhon peralta <jhonperalta13@gmail.com>
2024-01-19 17:22:09 -0500

    Add missing parameter in TokenExpiredException constructor

--- src/main/java/com/alcadia/bovid/Exception/TokenExpiredException.java
+++ src/main/java/com/alcadia/bovid/Exception/TokenExpiredException.java
@@ -3,6 +3,7 @@ package com.alcadia.bovid.Exception;
 public class TokenExpiredException extends RuntimeException {
 
     public TokenExpiredException(String message, Throwable expiredOn) {
+        
         super(message, expiredOn);
 
     }
