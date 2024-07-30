79afe88b19d475d608b0d39a6d5e6464ba3618d3
Jhon peralta <jhonperalta13@gmail.com>
2024-07-29 22:11:25 -0500

    chore: Update SecurityConfiguration to allow "funcionario" role access to "/admin/**"

--- src/main/java/com/alcadia/bovid/Configuration/SecurityConfiguration.java
+++ src/main/java/com/alcadia/bovid/Configuration/SecurityConfiguration.java
@@ -59,7 +59,8 @@ public class SecurityConfiguration {
             public CorsConfiguration getCorsConfiguration(@NonNull HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
                 config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:4200",
-                        "http://localhost:5173/", "http://localhost:8000/", "http://localhost:8000",
+                        "http://localhost:5173/","http://192.168.0.220/",
+                         "http://localhost:8000/", "http://localhost:8000",
                         "http://localhost:48496", "https://665922d5497f3aaadbaaf8b0--melodic-halva-c4b1b1.netlify.app/",
                         "https://665922d5497f3aaadbaaf8b0--melodic-halva-c4b1b1.netlify.app",
                         "https://bovid.site/", "https://bovid.site", "http://bovid.site/",
